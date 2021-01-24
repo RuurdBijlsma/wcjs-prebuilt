55f618181131e74d1390f73510a75ba901d9476a
Ruurd Bijlsma <ruurd@bijlsma.dev>
2021-01-25 00:09:01 +0100

    Add hard-coded prebuilt node file

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-module.exports = require((process.env.WCJS_TARGET || './bin') + "/WebChimera.js.node");
+// module.exports = require((process.env.WCJS_TARGET || './bin') + "/WebChimera.js.node");
+module.exports = require("./lib/WebChimera.js.node");
